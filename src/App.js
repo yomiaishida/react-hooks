9d35175f744fabe084cf62b2ed10db4682532c30
Abayomi Aishida <yomicorperate@gmail.com>
2022-03-30 15:52:32 +0100

    useRef example 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
 
 export default function App() {
   return (
     <div className="mt-5">
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
