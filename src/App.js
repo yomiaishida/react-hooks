dedd9969dc6155c01a729a4ff574a596a8d750b8
Abayomi Aishida <yomicorperate@gmail.com>
2022-03-30 13:23:15 +0100

    Files cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import UseRefExample1 from './components/UseRefExample1'
-import UseRefExample2 from './components/UseRefExample2'
-import UseRefExample3 from './components/UseRefExample3'
-import UseMemoExample from './components/UseMemoExample'
-import UseCallbackExample from './components/UseCallbackExample'
-import CustomHookExample1 from './components/CustomHookExample1'
-import CustomHookExample2 from './components/CustomHookExample2'
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className='container mt-5'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/useref-example-1' element={<UseRefExample1 />} />
-          <Route path='/useref-example-2' element={<UseRefExample2 />} />
-          <Route path='/useref-example-3' element={<UseRefExample3 />} />
-          <Route path='/usememo-example' element={<UseMemoExample />} />
-          <Route path='/usecallback-example' element={<UseCallbackExample />} />
-          <Route
-            path='/custom-hook-example-1'
-            element={<CustomHookExample1 />}
-          />
-          <Route
-            path='/custom-hook-example-2'
-            element={<CustomHookExample2 />}
-          />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <div className="mt-5">
+      <h1>Hello</h1>
+    </div>
+  );
 }
-
-export default App
